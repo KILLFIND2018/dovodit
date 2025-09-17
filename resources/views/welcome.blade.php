c02f552480e732867d7c2527be302054703e79f7
Gulyaev Martinas <killfind@yandex.ru>
2025-09-17 22:21:10 +0300

    Start Laravel 12 + Bootstrap 5.3

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Довод IT</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    </head>
+    <body>
+    <h1>Dovod IT</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
+    </body>
+</html>
